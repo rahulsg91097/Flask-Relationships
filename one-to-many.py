961cd1c4785bedd8eca3ac57b7e7cfd5c5ed9e20
Grahulprdxn <rahulg@prdxn.com>
2024-04-22 07:13:15 +0530

    Added one-to-many relationship file

--- one-to-many.py
+++ one-to-many.py
@@ -10,6 +10,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     profile = db.relationship('Profile', uselist=False, back_populates='user')
+    posts = db.relationship("Post", backref="author", lazy=True)
 
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,8 +18,47 @@ class Profile(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True)
     user = db.relationship('User', uselist=False, back_populates='profile')
 
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 with app.app_context():
     db.create_all()
+
+
+@app.route('/post-add')
+def addPost():
+    user = User.query.get(1)
+    posts = [
+        Post(title='post title2', description="Post descriptionsss2", author=user),
+        Post(title='post title3', description="Post descriptionsss3", author=user),
+        Post(title='post title4', description="Post descriptionsss4", author=user)
+    ]
+    for post in posts:
+        db.session.add(post)
+
+    db.session.add(post)
+    db.session.commit()
+    return jsonify({'message': 'Post added Successfully'})
+
+
+@app.route('/posts')
+def getPosts():
+    posts = Post.query.all()
+    post_list = []
+    for post in posts:
+        post_data = {
+            'id': post.id,
+            'title': post.title,
+            'description': post.description,
+            'author': post.author.name
+        }
+        post_list.append(post_data)
+
+    return jsonify(post_list)
+
 
 @app.route('/user-add')
 def addUser():
@@ -38,8 +78,15 @@ def getUser():
         user_data = {
             'id': user.id,
             'name': user.name,
-            'bio': user.profile.bio
+            'bio': user.profile.bio,
+            'posts': []
         }
+        for post in user.posts:
+            post_data = {
+                'title':post.title,
+                'description':post.description,
+            }
+            user_data['posts'].append(post_data)
         user_list.append(user_data)
 
     return jsonify(user_list)
